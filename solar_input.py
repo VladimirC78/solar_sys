fc8a1100930a235874174d4f21dd08f1d9604bbd
Kirill <kirill200521008@gmail.com>
2023-11-23 15:17:40 +0300

    исправлены классы звезд и планет

--- solar_input.py
+++ solar_input.py
@@ -4,7 +4,6 @@
 from solar_objects import Star, Planet
 from solar_vis import DrawableObject
 
-
 def read_space_objects_data_from_file(input_filename):
     """Cчитывает данные о космических объектах из файла, создаёт сами объекты
     и вызывает создание их графических образов
@@ -13,6 +12,7 @@ def read_space_objects_data_from_file(input_filename):
 
     **input_filename** — имя входного файла
     """
+
     objects = []
     with open(input_filename, 'r') as input_file:
         for line in input_file:
@@ -21,10 +21,12 @@ def read_space_objects_data_from_file(input_filename):
 
             object_type = line.split()[0].lower()
             if object_type == "star":
-                star = Star(parse_star_parameters(line))
+                star = Star()
+                parse_star_parameters(line, star)
                 objects.append(star)
             elif object_type == "planet":
-                planet = Planet(parse_planet_parameters(line))
+                planet = Planet()
+                parse_planet_parameters(line, planet)
                 objects.append(planet)
             else:
                 print("Unknown space object")
@@ -32,29 +34,46 @@ def read_space_objects_data_from_file(input_filename):
     return [DrawableObject(obj) for obj in objects]
 
 
-def parse_star_parameters(line):
-    splitted = line.split(' ').lower()
-    r = splitted[1]
-    color = splitted[2]
-    m = splitted[3]
-    x = splitted[4]
-    y = splitted[5]
-    vx = splitted[6]
-    vy = splitted[7]
-    return r, color, m, x, y, vx, vy
+def parse_star_parameters(line, star):
+    """Считывает данные о звезде из строки.
 
+    Входная строка должна иметь слеюущий формат:
 
-def parse_planet_parameters(line):
-    splitted = line.split(' ').lower()
-    r = splitted[1]
-    color = splitted[2]
-    m = splitted[3]
-    x = splitted[4]
-    y = splitted[5]
-    vx = splitted[6]
-    vy = splitted[7]
-    return r, color, m, x, y, vx, vy
+    Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
 
+    Здесь (x, y) — координаты зведы, (Vx, Vy) — скорость.
+
+    Пример строки:
+
+    Star 10 red 1000 1 2 3 4
+
+    Параметры:
+
+    **line** — строка с описание звезды.
+
+    **star** — объект звезды.
+    """
+    pass  # FIXME: допишите парсер
+
+def parse_planet_parameters(line, planet):
+    """Считывает данные о планете из строки.
+    Входная строка должна иметь слеюущий формат:
+
+    Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
+
+    Здесь (x, y) — координаты планеты, (Vx, Vy) — скорость.
+
+    Пример строки:
+
+    Planet 10 red 1000 1 2 3 4
+
+    Параметры:
+
+    **line** — строка с описание планеты.
+
+    **planet** — объект планеты.
+    """
+    pass  # FIXME: допишите парсер
 
 def write_space_objects_data_to_file(output_filename, space_objects):
     """Сохраняет данные о космических объектах в файл.
@@ -79,4 +98,3 @@ def write_space_objects_data_to_file(output_filename, space_objects):
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
-
