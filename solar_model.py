5020e208bb96ef37e2d5ca4c72eed69486624843
Kirill <kirill200521008@gmail.com>
2023-11-29 20:54:05 +0300

    исправил функцию вычисления производной на более точную с помощью метода рунге-кутты

--- solar_model.py
+++ solar_model.py
@@ -22,10 +22,33 @@ def calculate_force(body, space_objects):
 def move_space_object(body, dt):
     ax = body.Fx/body.m
     ay = body.Fy/body.m
-    body.x += body.Vx * dt
+    body.x += body.Vx * dt + ax * dt ** 2 / 2
     body.Vx += ax * dt
-    body.y += body.Vy * dt
+    body.y += body.Vy * dt + ay * dt ** 2 / 2
     body.Vy += ay * dt
+
+
+def RK4(space_objects, dt):
+    k1 = space_objects
+    for body in k1:
+        calculate_force(body, space_objects)
+    for body in k1:
+        move_space_object(body, dt)
+    k2 = space_objects
+    for body in k2:
+        calculate_force(body, k1)
+    for body in k2:
+        move_space_object(body, dt/2)
+    k3 = space_objects
+    for body in k3:
+        calculate_force(body, k2)
+    for body in k3:
+        move_space_object(body, dt/2)
+    for i in range(len(space_objects)):
+        space_objects[i].x += dt/6 * (space_objects[i].Vx + 2 * k1[i].Vx + 2 * k2[i].Vx + k3[i].Vx)
+        space_objects[i].y += dt/6 * (space_objects[i].Vy + 2 * k1[i].Vy + 2 * k2[i].Vy + k3[i].Vy)
+        space_objects[i].Vx += dt/6 * (space_objects[i].Fx/space_objects[i].m + 2 * k1[i].Fx/k1[i].m + 2 * k2[i].Fx/k2[i].m + k3[i].Fx/k3[i].m)
+        space_objects[i].Vy += dt/6 * (space_objects[i].Fy/space_objects[i].m + 2 * k1[i].Fy/k1[i].m + 2 * k2[i].Fy/k2[i].m + k3[i].Fy/k3[i].m)
 
 
 def recalculate_space_objects_positions(space_objects, dt):
@@ -33,6 +56,8 @@ def recalculate_space_objects_positions(space_objects, dt):
         calculate_force(body, space_objects)
     for body in space_objects:
         move_space_object(body, dt)
+
+    #RK4(space_objects, dt)
 
 
 if __name__ == "__main__":
