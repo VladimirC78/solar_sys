6e1995e815744519ebbdabad3643faefe4a22290
Kirill <kirill200521008@gmail.com>
2023-11-29 22:36:09 +0300

    добавил столкновение, при столкновении одна планета исчезает, и вторая движется с измененной массой

--- solar_main.py
+++ solar_main.py
@@ -75,7 +75,6 @@ def open_file():
     in_filename = "solar_system.txt"
     space_objects = read_space_objects_data_from_file(in_filename)
     max_distance = max([max(abs(obj.obj.x), abs(obj.obj.y)) for obj in space_objects])
-    print(max_distance)
     calculate_scale_factor(max_distance)
 
 
--- solar_model.py
+++ solar_model.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 # license: GPLv3
 import math
-
+from typing import List
+import random
+from solar_objects import Planet
 gravitational_constant = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
 
@@ -50,9 +52,31 @@ def RK4(space_objects, dt):
         space_objects[i].Vx += dt/6 * (space_objects[i].Fx/space_objects[i].m + 2 * k1[i].Fx/k1[i].m + 2 * k2[i].Fx/k2[i].m + k3[i].Fx/k3[i].m)
         space_objects[i].Vy += dt/6 * (space_objects[i].Fy/space_objects[i].m + 2 * k1[i].Fy/k1[i].m + 2 * k2[i].Fy/k2[i].m + k3[i].Fy/k3[i].m)
 
+def hit_test_space_objects(body, space_objects):
+    scale_factor = 0.0000000003
 
+    for obj in space_objects:
+        if body != obj:
+            x = obj.x - body.x
+            y = obj.y - body.y
+            r_obj = obj.R / scale_factor
+            r_body = body.R / scale_factor
+            if (obj.alive == 1) and (float((x**2 + y**2)) <= float((r_obj + r_body) ** 2)):
+                body.Vx = (body.m * body.Vx + obj.m * obj.Vx) / (body.m + obj.m)
+                body.Vy = (body.m * body.Vy + obj.m * obj.Vy) / (body.m + obj.m)
+                body.Fx = body.Fy = 0
+                body.x = (body.m * body.x + obj.m * obj.x) / (body.m + obj.m)
+                body.y = (body.m * body.y + obj.m * obj.y) / (body.m + obj.m)
+                body.R = (2 * (body.m + obj.m) / (body.m / body.R**3 + obj.m / obj.R**3)) ** (1 / 3)
+                body.m = body.m + obj.m
+                body.color = (random.randint(100, 255), random.randint(100, 255), random.randint(100, 255))
+                obj.alive = 0
 def recalculate_space_objects_positions(space_objects, dt):
     RK4(space_objects, dt)
+    for body in space_objects:
+        if body.alive:
+            hit_test_space_objects(body, space_objects)
+
 
 
 if __name__ == "__main__":
